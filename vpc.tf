e58f3d458fe3493f688ad8a42e27c97d68d8618c
Dimitar Dimitrov <ddimitrov@mirantis.com>
2024-04-23 23:35:31 +0300

    PRODENG-2584 Distributing public subnets 1 per AZ
    Signed-off-by: Dimitar Dimitrov <ddimitrov@mirantis.com>

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ data "aws_availability_zones" "available" {
 
 locals {
   // try to get as many azs as needed to stripe subnets across them.
-  az_subsets = slice(data.aws_availability_zones.available.names, 0, 1)
+  az_subsets = slice(data.aws_availability_zones.available.names, 0, var.network.public_subnet_count)
 }
 
 module "vpc" {
@@ -14,9 +14,8 @@ module "vpc" {
   name = var.name
   cidr = var.network.cidr
 
-  azs             = local.az_subsets
-  public_subnets  = [for i in range(var.network.public_subnet_count) : cidrsubnet(var.network.cidr, 4, i)]
-  private_subnets = [for i in range(var.network.public_subnet_count, var.network.private_subnet_count + var.network.public_subnet_count) : cidrsubnet(var.network.cidr, 4, i)]
+  azs            = local.az_subsets
+  public_subnets = [for i in range(var.network.public_subnet_count) : cidrsubnet(var.network.cidr, 4, i)]
 
   enable_nat_gateway = var.network.enable_nat_gateway
   enable_vpn_gateway = var.network.enable_vpn_gateway
