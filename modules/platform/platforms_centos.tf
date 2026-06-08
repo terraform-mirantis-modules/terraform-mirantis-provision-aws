1993e8a31911147f398e1e6fc289961fac3fe913
James Nesbitt <jnesbitt@mirantis.com>
2026-06-08 11:13:53 +0300

    feat(platform): add CentOS Stream 9 and 10 platform entries

--- modules/platform/platforms_centos.tf
+++ modules/platform/platforms_centos.tf
@@ -16,5 +16,21 @@ locals {
       "ssh_user" : "centos",
       "ssh_port" : 22
     },
+    "centos_stream_9" : {
+      "ami_name" : "CentOS Stream 9 x86_64 *",
+      "owner" : "125523088429",
+      "interface" : "eth0"
+      "connection" : "ssh",
+      "ssh_user" : "ec2-user",
+      "ssh_port" : 22
+    },
+    "centos_stream_10" : {
+      "ami_name" : "CentOS Stream 10 x86_64 *",
+      "owner" : "125523088429",
+      "interface" : "eth0"
+      "connection" : "ssh",
+      "ssh_user" : "ec2-user",
+      "ssh_port" : 22
+    },
   }
 }
